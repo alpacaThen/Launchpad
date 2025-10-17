bc47a9dfc84f26ac49436b22b7eb76e637f4c397
papp.kristof <papp.kristof@ma-coding.com>
2025-10-17 13:50:40 +0200

    format

--- Launchpad/Components/ResponsiveWindow.swift
+++
@@ -1,8 +0,0 @@
-import AppKit
-
-final class ResponsiveWindow: NSWindow {
-   override var canBecomeKey: Bool { true }
-   override var canBecomeMain: Bool { true }
-   override var acceptsFirstResponder: Bool { true }
-
-}
--- Launchpad/Components/WindowAccessor.swift
+++ Launchpad/Components/WindowAccessor.swift
@@ -1,31 +1,35 @@
 import SwiftUI
 
 struct WindowAccessor: NSViewRepresentable {
-    func makeNSView(context: Context) -> NSView {
-        let view = NSView()
-        DispatchQueue.main.async {
-            guard let currentWindow = view.window else { return }
-            
-            let newWindow = ResponsiveWindow(
-                contentRect: currentWindow.frame,
-                styleMask: currentWindow.styleMask,
-                backing: .buffered,
-                defer: false
-            )
-            
-            newWindow.contentView = currentWindow.contentView
-            newWindow.titleVisibility = .hidden
-            newWindow.styleMask.remove([.resizable, .titled])
-            newWindow.level = .floating
-            newWindow.setFrame(NSScreen.main?.frame ?? .zero, display: true)
-            newWindow.backgroundColor = .clear
-            newWindow.isOpaque = false
-            newWindow.makeKeyAndOrderFront(nil)
-            
-            currentWindow.close()
-        }
-        return view
-    }
-    
-    func updateNSView(_ nsView: NSView, context: Context) {}
+   func makeNSView(context: Context) -> NSView {
+      let view = NSView()
+      DispatchQueue.main.async {
+         guard let currentWindow = view.window else { return }
+
+         let newWindow = ResponsiveWindow(
+            contentRect: currentWindow.frame,
+            styleMask: currentWindow.styleMask,
+            backing: .buffered,
+            defer: false
+         )
+
+         newWindow.contentView = currentWindow.contentView
+         newWindow.titleVisibility = .hidden
+         newWindow.styleMask.remove([.resizable, .titled])
+         newWindow.level = .floating
+         newWindow.setFrame(NSScreen.main?.frame ?? .zero, display: true)
+         newWindow.makeKeyAndOrderFront(nil)
+
+         currentWindow.close()
+      }
+      return view
+   }
+
+   func updateNSView(_ nsView: NSView, context: Context) {}
+
+   final class ResponsiveWindow: NSWindow {
+      override var canBecomeKey: Bool { true }
+      override var canBecomeMain: Bool { true }
+      override var acceptsFirstResponder: Bool { true }
+   }
 }
