0cb3099721ee6d60c680b9c480f87a4736f95b63
papp.kristof <papp.kristof@ma-coding.com>
2025-10-18 18:11:13 +0200

    Unhide

--- Launchpad/AppDelegate.swift
+++ Launchpad/AppDelegate.swift
@@ -3,13 +3,15 @@ import AppKit
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
    private var isCurrentlyHidden = true
-
+   
    func applicationDidHide(_ notification: Notification) {
+      print("Hiding Launchpad.")
       isCurrentlyHidden = true
    }
-
+   
    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
-      if isCurrentlyHidden {
+      print("Reopening Launchpad.")
+      if isCurrentlyHidden || !flag {
          isCurrentlyHidden = false
          return true
       } else {
