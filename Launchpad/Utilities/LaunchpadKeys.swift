467e6e4a9f9d460d7a8d8666b24329b705acba36
papp.kristof <papp.kristof@ma-coding.com>
2025-11-04 16:30:17 +0100

    preactivated

--- Launchpad/Utilities/LaunchpadKeys.swift
+++ Launchpad/Utilities/LaunchpadKeys.swift
@@ -1,17 +1,15 @@
 import Foundation
 
 class LaunchpadKeys {
-   private static let productKeys = ["BASIC", "PRO", "PREMIUM"]
-
    public static func isActivated(key: String) -> Bool {
-      return key == productKeys[0] || key == productKeys[1] || key == productKeys[2]
+      return true
    }
 
    public static func isPro(key: String) -> Bool {
-      return key == productKeys[1] || key == productKeys[2]
+      return true
    }
 
    public static func isPremium(key: String) -> Bool {
-      return key == productKeys[2]
+      return true
    }
 }
