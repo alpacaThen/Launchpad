5350bd039f51c470bff7e148384eb81f9e76bf37
papp.kristof <papp.kristof@ma-coding.com>
2025-11-04 18:54:51 +0100

    format

--- Launchpad/Utilities/LaunchpadKeys.swift
+++ Launchpad/Utilities/LaunchpadKeys.swift
@@ -4,11 +4,11 @@ class LaunchpadKeys {
    public static func isActivated(key: String) -> Bool {
       return true
    }
-
+   
    public static func isPro(key: String) -> Bool {
       return true
    }
-
+   
    public static func isPremium(key: String) -> Bool {
       return true
    }
